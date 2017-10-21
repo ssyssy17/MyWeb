1c92e6b6eb2f1dfb524bb2e924961111d02a1894
ssy <ssyssy17@nate.com>
2017-10-21 10:59:37 +0900

    1

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://ssyssy17.run.goorm.io/PHP_SSY/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
